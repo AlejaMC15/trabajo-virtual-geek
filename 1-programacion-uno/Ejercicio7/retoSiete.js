f4ba32133f2c606f40e394bca62c15b711f58734
AlejaMC15 <aleja-mc@hotmail.com>
2020-03-16 15:45:59 -0500

    se modifico el ejercicio cuatro

--- 1-programacion-uno/Ejercicio7/retoSiete.js
+++ 1-programacion-uno/Ejercicio7/retoSiete.js
@@ -6,6 +6,6 @@ const contnt1= ' 1';
 const content2= ' 2';
 const content3= ' 3';
 
-li1Element.innerHTML = content1;
+li1Element.innerHTML = contnt1;
 li2Element.innerHTML = content2;
 li3Element.innerHTML = content3;
