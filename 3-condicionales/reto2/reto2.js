9defb648f613b535f2c7c51df5144e74069a852f
AlejaMC15 <aleja-mc@hotmail.com>
2020-03-16 23:33:10 -0500

    finalización string 1

---
+++ 3-condicionales/reto2/reto2.js
@@ -0,0 +1,11 @@
+'use strict';
+const h1dElement = document.querySelector('.h1');
+var nombre = '';
+
+nombre = prompt("ingresa el nombre del usuario ");
+if (nombre === "Julieth" || nombre === "Aleja") {
+    h1dElement.innerHTML = 'Bienvenidos';
+}
+else if (nombre != "Julieth" || nombre != "Aleja") {
+    h1dElement.innerHTML = 'Nombre de usuario incorrecto';
+}
---
+++ 3-condicionales/reto7/reto7.js
@@ -0,0 +1,4 @@
+var n = parseInt(prompt("Ingrese un año para saber si es bisiesto "));
+var age = n + (n % 4);
+
+console.log("El proximo año bisciesto es: " + age);
